e241ee8405a64e7c3ecdec988c2a3e0369125dd1
Thytu <valentin.de-matos@epitech.eu>
2021-12-13 10:34:16 +0100

    doc: add doc to plot_single_value

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,18 @@
 import matplotlib.pyplot as plt
 
-def plot_single_value(value, label, title, xlabel, ylabel):
+def plot_single_value(value, label, title, xlabel, ylabel) -> None:
+    """
+    Plot a single value with a given label.
+
+    :param value: The value to plot.
+    :param label: The label of the value.
+    :param title: The title of the plot.
+    :param xlabel: The x-axis label.
+    :param ylabel: The y-axis label.
+
+    :return: None
+    """
+
     plt.plot(value, label=label)
 
     plt.xlabel(xlabel)
